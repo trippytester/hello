29d2b03e41810032de6d94ef627d4ebfcd5eccfc
trippytester <amitabh.nampalliwar01@gmail.com>
2019-08-27 17:25:56 +0530

    yeyyyyyyyyy

---
+++ src/test/java/mavendemofinal/Demo.java
@@ -0,0 +1,14 @@
+package mavendemofinal;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void get2()
+	{
+		Reporter.log("hey",true);
+	}
+
+}
